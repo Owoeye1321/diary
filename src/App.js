70b8c25e98c48173411d5a9863c5f1009ee9162e
Owoeye1321 <owoeye1321@gmail.com>
2022-04-29 12:03:20 -0700

    started frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './frontend/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+    <div>
+       
+        
+    <Router> 
+      
+            <Routes>
+
+        <Route exact path = '/' element = {<Home />}/>
+
+   
+       {/* <Route path='/aboutUs'  element = {<About/>}/>
+       
+        <Route path='/marketingService' element = {<MarketingService/>}/>
+        
+    
+            <Route path='/pricing'  element = {< Pricing  key = {products.id} products = {products} /> }/>
+        
+         
+        <Route  path="/contactUs"  element = {<ContactUs/>}/>
+
+        <Route  path="/cart"  element = {<Cart/>}/>
+
+        <Route  path="/login"  element = {<Login/>}/>
+
+        <Route  path="/signUp"  element = {<SignUp/>}/>
+        <Route  path="/simplePaystackPaymentPageForAddriggo"  element = {<App/>}/> */}
+
+        </Routes>
+
+ 
+        
+
+    
+    </Router>
+ 
+    </div>    
+
+
+
+
   );
 }
 
---
+++ src/frontend/home.js
@@ -0,0 +1,21 @@
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function Home(){
+    return(
+        <div>
+            <div className = 'row'>
+
+                <div className="col-sm-12 col-md-5 col-lg-5">
+                  <h1>This is home</h1>
+                </div>   
+
+                    <div className="col-sm-12 col-md-7 col-lg-7">
+                      <h1>This is home</h1>
+                    </div>
+
+            </div>
+        </div>
+    )
+}
+export default Home
