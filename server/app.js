ae744c0c992656d061802e78763f4b7ec79da742
Owoeye1321 <owoeye1321@gmail.com>
2022-05-05 18:48:20 -0700

    added new stuffs

--- server/app.js
+++ server/app.js
@@ -1,32 +1,15 @@
 const express = require('express')
-const { MongoClient } = require("mongodb");
 const app = express()
-app.use(express.json())
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-  }
-const uri = process.env.ATLAS_URI
+const PORT = process.env.PORT || 6000
+app.use('/login',require('./routes/login'))
+app.use('/signUp',require('./routes/signUp'))
+app.use('/insert',require('./routes/createUpdate'))
+app.use('/read', require('./routes/read'))
 
-const client = new MongoClient(uri);
-async function run() {
-    try {
-      await client.connect();
-      const database = client.db('diary');
-      const users = database.collection('user');
-      // Query for a movie that has the title 'Back to the Future'
-      const query = { title: 'gtwatt' };
-      const user = await movies.findOne(query);
-      console.log(movie);
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
-  }
-  run().catch(console.dir);
+app.use('*',( req, res)=>{
+ res.send('Hello there, you seem to be lost on this server')
+})
 
-// const PORT = process.env.PORT || 6000
-
-
-// app.listen(PORT, () => {
-//     console.log('Listening to port' + ' ' + PORT)
-//   })    
+app.listen(PORT, () => {
+    console.log('Listening to port' + ' ' + PORT)
+  })    
---
+++ server/routes/read.js
@@ -0,0 +1,27 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
+const express = require('express')
+const router = express.Router()
+router.post('/',(req,res)=>{
+    const uri = process.env.ATLAS_URI
+    const client = new MongoClient(uri);
+    async function run() {
+    try {
+      await client.connect();
+      const database = client.db('diary');
+      const users = database.collection('user');
+      // Query for a movie that has the title 'Back to the Future'
+      const query = { title: 'gtwatt' };
+      const user = await movies.findOne(query);
+      console.log(movie);
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+    res.send('hello read')
+})
+module.exports = router
