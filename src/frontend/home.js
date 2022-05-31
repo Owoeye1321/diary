d0ed769cb84aede3effbc4b778344abb6264385a
Owoeye1321 <owoeye1321@gmail.com>
2022-05-30 17:51:41 -0700

    new commit

--- src/frontend/home.js
+++ src/frontend/home.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Styles from './style.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPencil } from '@fortawesome/free-solid-svg-icons';
+import { faPencil ,  faBookmark} from '@fortawesome/free-solid-svg-icons';
 import axios from "axios";
 function Home(){
     const [color, setColor] = useState('#000000')
     const [txtcolor, setTxtColor ] = useState('white')
     const [body, setBody ] = useState([])
+    const [blockForI, setBlockForI] = useState('block')
+    const [noneForM, setNoneForM ] = useState('none')
     const [getMessage, setGetMessage ] = useState ('Content would be displayed soon')
  
 
@@ -29,9 +31,10 @@ function Home(){
             }
 
          }
+         fetchAll()
             const interval = setInterval (()=>{
                 fetchAll()
-            },1000)
+            },60000)
 
             return()=>{
                     clearInterval(interval)
@@ -41,12 +44,10 @@ function Home(){
 
     const setBodyMessageToRead = async (ObjectId)=>{
         const messageId = ObjectId
-        const result = await axios.post('/fetchBody',messageId)
+        console.log(messageId)
+        const result = await axios.post('/fetchBody',{messageId:messageId})
         if(result.data){
-            const details = result.data
-            details.map((key)=>{
-                setGetMessage(key.body)
-            })
+            setGetMessage(result.data.body)
         }
     }
 
@@ -57,8 +58,17 @@ function Home(){
             <div id = {Styles.desktop}>
             <div className = 'row' style = {{backgroundColor:'rgb(229, 214, 130)',height:'700px'}}>
                     <div style = {{width:'100%',padding:'20px 20px 0px 20px',height:'50px',marginBottom:'20px'}}>
-                                    <h1 style = {{color:'white',float:'left'}}> Notepad</h1>
-                                    <a style = {{float:'right',marginTop:'5px'}} href = '/text'><FontAwesomeIcon icon={faPencil}  size = 'xl' style = {{color:'white'}}/> </a>
+                        <div style={{float:"left"}}>
+                        <h1 style = {{color:'white',float:'left'}}> Notepad</h1>
+                        </div>
+                        <div style={{float:"right"}}>
+                                    <a style = {{float:'right',marginTop:'5px'}} href = '/text'>
+                                        <FontAwesomeIcon icon={faPencil}  size = 'xl' style = {{color:'white'}}/>
+                                         </a>
+
+                        </div>
+                                    
+                                    
                                 </div>
                         <div className="col-sm-12 col-md-5 col-lg-4">
                             <div id = {Styles.titleContent}>
@@ -68,8 +78,8 @@ function Home(){
                                                 <div key={key._id}
                                                 style ={{backgroundColor:color,color:txtcolor,marginBottom:'10px'}}
                                                  id = {Styles.pick}>
-                                                     <p  onClick={()=>setBodyMessageToRead(key._id)}>{key.title}</p>
-                                                      <p style = {{marginTop:'-20px'}}>{key.body}</p>
+                                                     <p  onClick={()=>{setBodyMessageToRead(key._id)}}>{key.title}</p>
+                                                      <p style = {{marginTop:'-20px',fontSize:'10px'}}>{key.body.substring(0,30)}</p>
 
                                                   </div>
                                                )
@@ -87,8 +97,8 @@ function Home(){
 
                             <div className="col-sm-12 col-md-7 col-lg-8">
                                 <div id = {Styles.contentDisc}>
-                                    <div id = {Styles.readScroll}>
-                                        <h1>{getMessage}</h1>
+                                    <div className="p-2" id = {Styles.readScroll}>
+                                        <h6>{getMessage}</h6 >
                                        
                                     </div>
                                       
@@ -101,17 +111,26 @@ function Home(){
             <div id = {Styles.mobile}>
             <div style = {{width:'100%',padding:'20px 20px 0px 20px'}}>
                     <h1 style = {{color:'black',float:'left'}}> Notepad</h1>
+                    <a className="mx-2" style = {{float:'right',marginTop:'5px'}} href = '/'>
+                        <FontAwesomeIcon icon={faBookmark}  size = 'xl' style = {{color:'black'}}/>
+                        </a>
                     <a style = {{float:'right',marginTop:'5px'}} href = '/text'><FontAwesomeIcon icon={faPencil}  size = 'xl' style = {{color:'black'}}/> </a>
                 </div>
-            <div id = {Styles.titleContent}>
+            <div id = {Styles.titleContent} style = {{display:blockForI}}>
                                     <div id = {Styles.readScroll}> 
                                     { body ? body.map((key)=>{
                                                return(
                                                 <div key={key._id}
                                                 style ={{backgroundColor:color,marginBottom:'10px'}}
                                                  id = {Styles.pick}>
-                                                       <p  onClick={()=>setBodyMessageToRead(key._id)}>{key.title}</p>
-                                                      <p style = {{marginTop:'-20px'}}>{key.body}</p>
+                                                       <p  
+                                                       onClick={()=> { 
+                                                           setBodyMessageToRead(key._id);
+                                                           setBlockForI('none')
+                                                           setNoneForM('block')
+                                                           }}>
+                                                           {key.title}</p>
+                                                      <p style = {{marginTop:'-20px',fontSize:'10px'}}>{key.body.substring(0,30)}</p>
 
                                                   </div>
                                                )
@@ -122,6 +141,16 @@ function Home(){
                                              </div>}
                                         
                                     </div>
+                            </div>
+                            <div style = {{display:noneForM}} className  = 'm-3'>
+                                <div id = {Styles.contentDisc}>
+                                    <div className="p-2" id = {Styles.readScroll}>
+                                        <h6>{getMessage}</h6 >
+                                       
+                                    </div>
+                                      
+                                </div>
+                            
                             </div>
             </div>
         </div>
