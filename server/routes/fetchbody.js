60c167b3562685f17a149728b9dfd89e0363edbc
Owoeye1321 <owoeye1321@gmail.com>
2022-05-30 17:51:01 -0700

    new commit

--- server/routes/fetchbody.js
+++ server/routes/fetchbody.js
@@ -3,26 +3,25 @@ const uri = process.env.ATLAS_URI
 
 const router = require('express').Router()
    const { MongoClient, ServerApiVersion } = require('mongodb');
+   const ObjectId = require('mongodb').ObjectId;
  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 router.post('/',(req, res) =>{
    const sess = req.session
+   console.log(req.body)
    if(sess.username){
-       const getId = req.body.messageId
       const username = sess.username
       console.log(username)
          client.connect(async err => {
          const collection = client.db("diary").collection("notes");
-         const result = await collection.findOne({_id:getId}).toArray()
+         const getId = req.body.messageId
+         console.log(getId)
+         const result = await collection.findOne({_id:ObjectId(getId)})
             if(result){
+               console.log(result)
                res.json(result)
-               console.log('working on result')
-               result.map((key)=>{
-                  console.log(key._id)
-                        console.log(key.title)
-                  console.log(key.body)
-               })
             }else{
+               res.send('Unable to find notes with id')
                console.log('an error has occured')
             }
          });
