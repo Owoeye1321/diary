a989f6519840f06fa58e83daae3e71f85d316413
Owoeye1321 <owoeye1321@gmail.com>
2022-06-28 12:25:35 -0700

    new commit

--- server/routes/fetchbody.js
+++ server/routes/fetchbody.js
@@ -1,18 +1,13 @@
-if (process.env.NODE_ENV !== "production") require('dotenv').config();
-const uri = process.env.ATLAS_URI
-
 const router = require('express').Router()
-   const { MongoClient, ServerApiVersion } = require('mongodb');
    const ObjectId = require('mongodb').ObjectId;
- const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+   const client = require('../controller/client')
 
-router.post('/',(req, res) =>{
+router.post('/',async (req, res) =>{
    const sess = req.session
    console.log(req.body)
    if(sess.username){
       const username = sess.username
       console.log(username)
-         client.connect(async err => {
          const collection = client.db("diary").collection("notes");
          const getId = req.body.messageId
          sess.updateId = getId
@@ -25,7 +20,6 @@ router.post('/',(req, res) =>{
                res.send('Unable to find notes with id')
                console.log('an error has occured')
             }
-         });
       }else{
          res.send('invalid user')
          console.log('User authentication required')
