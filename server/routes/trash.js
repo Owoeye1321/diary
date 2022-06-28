419a62dcdabe6eb3b074135cb9b7e0fe0a568045
Owoeye1321 <owoeye1321@gmail.com>
2022-06-28 12:28:47 -0700

    new commit

--- server/routes/trash.js
+++ server/routes/trash.js
@@ -1,15 +1,10 @@
-if (process.env.NODE_ENV !== "production") require('dotenv').config();
-const uri = process.env.ATLAS_URI
-
 const router = require('express').Router()
-   const { MongoClient, ServerApiVersion } = require('mongodb');
    const ObjectId = require('mongodb').ObjectId;
- const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+   const client = require('../controller/client')
 
-router.post('/',(req, res) =>{
+router.post('/',async (req, res) =>{
     const sess = req.session
     if(sess.username){
-           client.connect(async err => {
            const collection = client.db("diary").collection("notes");
            const getId = req.body.trashingId
            const result = await collection.deleteOne({_id:ObjectId(getId)})
@@ -20,7 +15,6 @@ router.post('/',(req, res) =>{
                  res.send('Unable to find notes with id')
                  console.log('an error has occured')
               }
-           });
         }else{
            res.send('invalid user')
            console.log('User authentication required')
