847c0424a0ab221c6699c22aa17402ba09f06a13
Owoeye1321 <owoeye1321@gmail.com>
2022-06-28 12:28:11 -0700

    new commit

--- server/routes/signUp.js
+++ server/routes/signUp.js
@@ -1,64 +1,66 @@
-if (process.env.NODE_ENV !== "production") require('dotenv').config();
-const uri = process.env.ATLAS_URI
-
-const { MongoClient, ServerApiVersion } = require('mongodb')
    const validator = require('../controller/validator')
-      const router = require('express').Router()
-   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+       const router = require('express').Router()
+          const client = require('../controller/client')   
 
-router.post('/',(req, res) =>{
-   const sess = req.session
-      const username = req.body.details.username
-     const email = req.body.details.email
+   router.post('/',async (req, res) =>{
+     console.log(req.body)
+       const sess = req.session
+   const username = req.body.details.username
+       const email = req.body.details.email
    const password = req.body.details.password
 
    const details = {
-      username:username,
-          email:email,
-       password:password
+   username:username,
+      email:email,
+   password:password
    }
    const validationRule ={
-      "username":'required|string',
-      "email" :"required|email",
-      "password": "required|min:6"
-  }
-  validator(details, validationRule, {}, (err, status)=>{
-     if(!status){
-           console.log('An error has occured')
-                 console.log(err)
-        res.json({
-           success:"false",
-             message:"Invalid details",
-          data:{err}
-     })
-     }else{
+   "username":'required|string',
+   "email" :"required|email",
+   "password": "required|min:8"
+   }
+   validator(details, validationRule, {}, (err, status)=>{
+   if(!status){
+      console.log('An error has occured')
+            console.log(err)
+   res.json({
+      success:"false",
+         message:"Invalid details",
+      data:{err}
+   })
+   }else{
 
-client.connect(async err => {
-   console.log('mongodb database connected successfully')
+   const signUp = async()=>{
       const collection = client.db("diary").collection("user");
-
-        const result  = await collection.insertOne({username:username, email:email, password:password})
-        if(result){
-               sess.username = username
+      const check = await collection.findOne({username:username})
+      if(check){
+         res.send('exist')
+         console.log('user already exist')
+      }else{
+         const result  = await collection.insertOne({username:username, email:email, password:password})
+         if(result){
+            sess.username = username
                   console.log('user saved')
-             res.send('success')
-        }else{
-           res.send('invalid')
-           console.log('unable to save new user')
-        }
-
-      client.close();
-      });
-  
-     }
-  })
+            res.send('success')
+         }else{
+            res.send('invalid')
+            console.log('unable to save new user')
+         }
+      }
 
 
-   
+   }
+   signUp()
 
-})
+   }
+   })
 
-module.exports = router
+
+
+
+   })
+
+   module.exports = router
 
 
 
