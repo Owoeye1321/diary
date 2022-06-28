7f416c1bb4eaf6c746c866368001dabeeb148ff6
Owoeye1321 <owoeye1321@gmail.com>
2022-06-28 12:24:54 -0700

    new commit

--- server/routes/create.js
+++ server/routes/create.js
@@ -1,15 +1,11 @@
-if (process.env.NODE_ENV !== "production") require('dotenv').config();
- const uri = process.env.ATLAS_URI
-   const { MongoClient, ServerApiVersion } = require('mongodb')
-      const router = require('express').Router()
- const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-router.post('/',(req, res) =>{
+const router = require('express').Router()
+   const client = require('../controller/client')   
+      router.post('/',async (req, res) =>{
       const sess = req.session
          const title = req.body.details.title
         const body = req.body.details.body
       const user = sess.username
    if(user){
-        client.connect( async () => {
              const collection = client.db("diary").collection("notes");
           const result = await collection.insertOne({title:title, body:body, user:user})
         if(result){
@@ -19,8 +15,7 @@ router.post('/',(req, res) =>{
            console.log('An error has occured')
          res.send('error')
         }
-          client.close();
-        });
+       
 
    }else{
       console.log('Authentication required')
