2de20926bfab57e9aadca4cbcaa0b90904782fb4
Owoeye1321 <owoeye1321@gmail.com>
2022-08-21 23:39:39 -0700

    new commit

--- server/routes/fetchbody.js
+++ server/routes/fetchbody.js
@@ -1,29 +1,59 @@
 const router = require('express').Router()
    const ObjectId = require('mongodb').ObjectId;
-   const client = require('../controller/client')
+const { faExternalLink } = require('@fortawesome/free-solid-svg-icons');
+      const create = require('../model/createModel')
 
 router.post('/',async (req, res) =>{
-   const sess = req.session
-   console.log(req.body)
-   if(sess.username){
-      const username = sess.username
-      console.log(username)
-         const collection = client.db("diary").collection("notes");
-         const getId = req.body.messageId
-         sess.updateId = getId
-         console.log(getId)
-         const result = await collection.findOne({_id:ObjectId(getId)})
-            if(result){
-               console.log(result)
-               res.json(result)
+   if(req.session.username){
+      const username = req.session.username
+      req.session.updatedId = req.body.messageId
+         req.session.save((error,result)=>{
+            if(!error){
+               console.log('Updated id session has been saved', req.session)
+            create.findOne({_id:ObjectId(req.session.updatedId)},( innerError , finalData)=>{
+               if(!innerError){
+                  res.json(finalData)
+                  console.log('The data bod has been sent for editing', finalData)
+               }else{
+                  
+                  console.log('An error has occured')
+               }
+            })
             }else{
-               res.send('Unable to find notes with id')
-               console.log('an error has occured')
+               console.log('Unable to save session', error)
             }
-      }else{
-         res.send('invalid user')
-         console.log('User authentication required')
-      }
+            
+            // const findingDateToSend = await create.findOne({_id:ObjectId(getId)})
+            // if(findingDateToSend){
+            //    res.json(findingDateToSend)
+            //    console.log('The data bod has been sent for editing', findingDateToSend)
+            // }else{
+            //    res.send('error')
+            //    console.log('An error has occured')
+            // }
+         })
+   }
+   // const sess = req.session
+   // console.log(req.body)
+   // if(sess.username){
+   //    const username = sess.username
+   //    console.log(username)
+   //       const collection = client.db("diary").collection("notes");
+   //       const getId = req.body.messageId
+   //       sess.updateId = getId
+   //       console.log(getId)
+   //       const result = await collection.findOne({_id:ObjectId(getId)})
+   //          if(result){
+   //             console.log(result)
+   //             res.json(result)
+   //          }else{
+   //             res.send('Unable to find notes with id')
+   //             console.log('an error has occured')
+   //          }
+   //    }else{
+   //       res.send('invalid user')
+   //       console.log('User authentication required')
+   //    }
       
 })
 
