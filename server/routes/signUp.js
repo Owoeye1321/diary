fe28f5727add77d4f45778b6bac8cc5d6fcea99e
Owoeye1321 <owoeye1321@gmail.com>
2022-08-21 23:20:43 -0700

    new commit

--- server/routes/signUp.js
+++ server/routes/signUp.js
@@ -1,66 +1,68 @@
-   const validator = require('../controller/validator')
-       const router = require('express').Router()
-          const client = require('../controller/client')   
+const validator = require('../controller/validator')
+      const router = require('express').Router()
+       const userModel = require('../model/userModel')
 
-   router.post('/',async (req, res) =>{
-     console.log(req.body)
-       const sess = req.session
-   const username = req.body.details.username
-       const email = req.body.details.email
+router.post('/',async (req, res) =>{
+   console.log(req.body)
+      const username = req.body.details.username
+     const email = req.body.details.email
    const password = req.body.details.password
 
    const details = {
-   username:username,
-      email:email,
-   password:password
+      username:username,
+          email:email,
+       password:password
    }
    const validationRule ={
-   "username":'required|string',
-   "email" :"required|email",
-   "password": "required|min:8"
-   }
-   validator(details, validationRule, {}, (err, status)=>{
-   if(!status){
-      console.log('An error has occured')
-            console.log(err)
-   res.json({
-      success:"false",
-         message:"Invalid details",
-      data:{err}
-   })
-   }else{
+      "username":'required|string',
+      "email" :"required|email",
+      "password": "required|min:8"
+  }
+  validator(details, validationRule, {}, (err, status)=>{
+     if(!status){
+           console.log('An error has occured')
+                 console.log(err)
+        res.json({
+           success:"false",
+             message:"Invalid details",
+          data:{err}
+     })
+     }else{
+                  const signUp = new userModel({
+                     username:req.body.details.username,
+                     email: req.body.details.email,
+                     password: req.body.details.password,
+                 })
+             
+               //checking if a user exist
+                 userModel.exists({username:username}, (err, result)=>{
+                  if(result){
+                     res.send('exist')
+                     console.log('file exist')
+                  }else{
+                     console.log('file does not exist')
 
-   const signUp = async()=>{
-      const collection = client.db("diary").collection("user");
-      const check = await collection.findOne({username:username})
-      if(check){
-         res.send('exist')
-         console.log('user already exist')
-      }else{
-         const result  = await collection.insertOne({username:username, email:email, password:password})
-         if(result){
-            sess.username = username
-                  console.log('user saved')
-            res.send('success')
-         }else{
-            res.send('invalid')
-            console.log('unable to save new user')
-         }
-      }
+                     //saving to sign up page
+                       signUp.save().then((result)=>{
+                        res.send('success')
+                           console.log('added a new profile')
+                           console.log('file has been saved to database successfully')
+                
+                    }).catch((err)=>{
+                     console.log(err)
+                     res.send('invalid')
+                    })
+                    
+                  }
+                 })
+                 
+           
+            
+     }
+  })
+})
 
-
-   }
-   signUp()
-
-   }
-   })
-
-
-
-
-   })
-
-   module.exports = router
+module.exports = router
 
 
 
