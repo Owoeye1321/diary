3b683c7a4493adbe0bf383456ce8ab4e309d3f72
Owoeye1321 <owoeye1321@gmail.com>
2022-08-21 23:28:36 -0700

    new commit

--- server/routes/trash.js
+++ server/routes/trash.js
@@ -1,24 +1,21 @@
 const router = require('express').Router()
    const ObjectId = require('mongodb').ObjectId;
-   const client = require('../controller/client')
+   const create = require('../model/createModel')
 
 router.post('/',async (req, res) =>{
-    const sess = req.session
-    if(sess.username){
-           const collection = client.db("diary").collection("notes");
-           const getId = req.body.trashingId
-           const result = await collection.deleteOne({_id:ObjectId(getId)})
-              if(result){
-                 console.log(result)
-                 res.send('success')
-              }else{
-                 res.send('Unable to find notes with id')
-                 console.log('an error has occured')
-              }
-        }else{
-           res.send('invalid user')
-           console.log('User authentication required')
-        } 
+
+    if(req.session.username){
+      console.log(req.body)
+      const getId = req.body.trashingId
+         const deletingData = await create.deleteOne({_id:ObjectId(getId)})
+         if(deletingData){
+            res.send('success')
+            console.log('The data has been deleted successfully')
+         }else{
+            res.send('error')
+            console.log('The data could not be deleted')
+         }
+    }
 })
 
 
