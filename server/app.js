9e34449c0e6c03346e5d274313fdea736c3dd560
Owoeye1321 <owoeye1321@gmail.com>
2022-09-08 14:29:00 -0700

    new commit

--- server/app.js
+++ server/app.js
@@ -13,15 +13,15 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-  "Access-Control-Allow-Headers",
-  "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-  });
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header(
+//   "Access-Control-Allow-Headers",
+//   "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+//   });
        app.use(express.json())
          const PORT = process.env.PORT || 4040
        const oneDay = 1000 * 60 * 60 * 24;
