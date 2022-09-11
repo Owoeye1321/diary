7e93bc4c34a8970e30bb06017bc163ccdefceeec
Owoeye1321 <owoeye1321@gmail.com>
2022-09-11 05:02:06 -0700

    now commit

--- src/frontend/forgetpassword.js
+++ src/frontend/forgetpassword.js
@@ -44,7 +44,7 @@ function Forgetpassword() {
                 className="form-control"
               />
             </div>
-            <div style={{ fontSize: '10px' ,marginTop: '0px'}}>
+            <div style={{ fontSize: '10px' ,marginBottom: '5'}}>
                                     <center>
                                     <i style={{marginBottom:"5px",color:'red'}}>{error}</i>
                                     </center>
